931047bf3324c038a15862aa3551f6059f58a1b5
nicetoseeyou <hello4world@yeah.net>
2022-03-04 01:47:05 +0800

    add flatten

---
+++ src/nanva/pandas_learner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from text_utils import PrimitiveKVFormatter, TextUtils
+
+# https://www.gairuo.com/file/data/dataset/GDP-China.csv
+
+
+df = pd.DataFrame({
+    "id": [1, 2],
+    "name": ["Kevin", "Jenny"],
+    "address": [{"hometown": "Meizhou", "work": "Guangzhou"}, {"hometown": "Hangzhou", "work": "Guangzhou"}],
+    "contact": [{"mobile": ["+86 16888", "+86 168888"], "mail": "niceman@nice.lab"},
+                {"mobile": ["+86 16666", "+86 166666"], "mail": "nicelady@nice.lab"}]
+})
+
+df['contact'] = df['contact'].apply(
+    lambda c: TextUtils.flatten_dict(c, formatter=PrimitiveKVFormatter(), array_index_start=1))
+
+with pd.option_context("expand_frame_repr", False,
+                       "display.max_rows", None,
+                       "display.max_colwidth", None):
+    print(df)
