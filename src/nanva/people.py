97e2ee56ca94c8faff07a6bc8a823c6f77a05f34
nicetoseeyou <hello4world@yeah.net>
2022-03-04 01:42:44 +0800

    delete unused scripts

--- src/nanva/people.py
+++
@@ -1,8 +0,0 @@
-class People:
-    def __init__(self, name, mail):
-        self.name = name
-        self.mail = mail
-
-    def info(self):
-        print(f"Name {self.name}, mail {self.mail}")
-        return None
